b423d6c9474dc828da1c8515f046b050e0171938
mtotbagi <55552289+mtotbagi@users.noreply.github.com>
2023-04-16 19:28:10 +0200

    Now dark_market returns both lists

--- dark_market.py
+++ dark_market.py
@@ -3,9 +3,9 @@ import numpy as np
 import time
 import random
 
-MAX_VALUE = 8
+VALUE_BITWIDTH = 4
+MAX_VALUE = 2**VALUE_BITWIDTH - 1
 MIN_VALUE = 0
-VALUE_BITWIDTH = 3
 
 MAXLENGTH = 2**3
 
@@ -20,8 +20,8 @@ def sum(list):
         r += val
     return r
 
-@fhe.compiler({"s" : "encrypted", "b" : "encrypted"})
-def dark_market(s, b):
+@fhe.compiler({"s" : "encrypted", "b" : "encrypted", "r" : "encrypted"})
+def dark_market(s, b, r):
     sellVol = sum(s)
     buyVol = sum(b)
     transVol = min(sellVol, buyVol)
@@ -34,6 +34,7 @@ def dark_market(s, b):
         #s[i] = ( ( leftVol - s[i] ) * z_2 + s[i] ) * (1 - z_1)
         s[i] = min(s[i], leftVol)
         leftVol -= s[i]
+        r[i] = s[i]
 
         #value_to_lookup = min(leftVol, s[i]) + 2**VALUE_BITWIDTH * (1 - (leftVol >= 0))
         #s[i] = select_lut[value_to_lookup]
@@ -43,11 +44,12 @@ def dark_market(s, b):
     for i in range(MAXLENGTH):
         b[i] = min(b[i], leftVol)
         leftVol -= b[i]
+        r[i+MAXLENGTH] = b[i]
 
     #Todo: return both lists at the same time
-    return b
+    return r
 
-def clean_part(s,b):
+def clean_part(s,b, r):
     print(s, b)
     CONFIGURATION = fhe.Configuration(
         enable_unsafe_features=True,
@@ -55,17 +57,22 @@ def clean_part(s,b):
         insecure_key_cache_location=".keys",
     )
 
-    inputset = [ ([1,2,3,0,0,0,0,0],[5,1,0,0,0,0,0,0]) , ([0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]) ]
+    inputset = [([random.randint(0, MAX_VALUE) for i in range(MAXLENGTH)], [random.randint(0, MAX_VALUE) for i in range(MAXLENGTH)], [0 for i in range(2*MAXLENGTH)]) , 
+                ([MAX_VALUE for i in range(MAXLENGTH)],[MAX_VALUE for i in range(MAXLENGTH)], [0 for i in range(2*MAXLENGTH)]),
+                ([MAX_VALUE for i in range(MAXLENGTH)],[MAX_VALUE for i in range(MAXLENGTH)], [0 for i in range(2*MAXLENGTH)])]
 
     circuit = dark_market.compile(inputset, CONFIGURATION)
 
-    res = circuit.encrypt_run_decrypt(s,b)
+    res = list(circuit.encrypt_run_decrypt(s,b,r))
 
     print(res)
+    print(circuit)
 
-s = [1,2,3] + [0] * (MAXLENGTH - 3)
-b = [2,2] + [0] * (MAXLENGTH - 2)
-
-clean_part(s, b)
-res = dark_market(s, b)
+#s = [1,2,3] + [0] * (MAXLENGTH - 3)
+#b = [2,2] + [0] * (MAXLENGTH - 2)
+s = [random.randint(0, MAX_VALUE) for i in range(MAXLENGTH)]
+b = [random.randint(0, MAX_VALUE) for i in range(MAXLENGTH)]
+r = [0 for i in range(2*MAXLENGTH)]
+clean_part(s, b, r)
+res = dark_market(s, b, r)
 print(res)
