03b6dd5dac392cd972f2447e8fcb0d1446a78601
mtotbagi <mtotbagi@gmail.com>
2023-05-14 12:21:03 +0200

    new impl

--- volume_match_impl/src/main.rs
+++ volume_match_impl/src/main.rs
@@ -1,17 +1,16 @@
 #![allow(non_snake_case)]
-#![allow(unused_assignments)]
-#![allow(unused_variables)]
 #![allow(dead_code)]
+#![allow(unused)]
 use rand::Rng;
 
 pub mod integer_padded_paral;
 pub mod high_api;
-pub mod integer_paral;
+pub mod full_paral;
 pub mod integer_u16_paral;
 pub mod logging;
 
-const MAXLISTLENGTH : usize = 500; //500;
-const MAXVALUE : u16 = 100; //100;
+const MAXLISTLENGTH : usize = 10; //500;
+const MAXVALUE : u16 = 10; //100;
 
 fn main() {
     for _ in 1..2 {
@@ -29,8 +28,8 @@ fn setupAndRun()
     // Define varibles
 
     //u16 for high_api
-    //let mut s_clear : Vec<u16> = vec![0; MAXLISTLENGTH];
-    //let mut b_clear : Vec<u16> = vec![0; MAXLISTLENGTH];
+    let mut s_clear : Vec<u16> = vec![0; MAXLISTLENGTH];
+    let mut b_clear : Vec<u16> = vec![0; MAXLISTLENGTH];
 
     //u16 for integer_u16
     let mut s_clear64_16 : Vec<u64> = vec![0; MAXLISTLENGTH];
@@ -59,7 +58,7 @@ fn setupAndRun()
     //println!("Input for integer_u16_paral : \n s = {s_clear64_16:?} \n b = {b_clear64_16:?}");
     //println!("Input for high_api_paral : \n s = {s_clear64:?} \n b = {b_clear64:?}");
     //println!("Input for integer_padded_paral : \n s = {s_clear64_p:?} \n b = {b_clear64_p:?}");
-    //println!("Input for integer_paral : \n s = {s_clear:?} \n b = {b_clear:?}");
+    println!("Input for integer_paral : \n s = {s_clear64:?} \n b = {b_clear64:?}");
 
     // Call to algos  //we time inside
 
@@ -67,8 +66,11 @@ fn setupAndRun()
     //drop(s_clear64_16);drop(b_clear64_16);
     //high_api::run(&mut s_clear, &mut b_clear, num_block);
     //drop(s_clear);drop(b_clear);
-    integer_padded_paral::run(&mut s_clear64_p, &mut b_clear64_p, num_block);
-    drop(s_clear64_p);drop(b_clear64_p);
+    //integer_padded_paral::run(&mut s_clear64_p, &mut b_clear64_p, num_block);
+    full_paral::run(&mut s_clear64, &mut b_clear64, num_block);
+    println!("Output for integer_paral : \n s = {s_clear64:?} \n b = {b_clear64:?}");
+
+    //drop(s_clear64_p);drop(b_clear64_p);
     //::run(&mut s_clear64, &mut b_clear64, num_block, MAXLISTLENGTH);
     //drop(s_clear64);drop(b_clear64);
 }
