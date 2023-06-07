4f3d09bbda79051ec53e0253fa924a99f1aa434d
mtotbagi <mtotbagi@gmail.com>
2023-06-07 18:10:56 +0200

    Cleaned up code, renamed stuff for readability

--- volume_match_impl/src/full_paral.rs
+++ volume_match_impl/src/full_paral.rs
@@ -1,36 +1,29 @@
 #![allow(non_snake_case)]
 use tfhe::integer::RadixClientKey;
 use tfhe::integer::{ServerKey, gen_keys_radix,  ciphertext::BaseRadixCiphertext};
-use tfhe::shortint::{CiphertextBase, ciphertext::KeyswitchBootstrap};
+use tfhe::shortint::{CiphertextBase, ciphertext::KeyswitchBootstrap, parameters::PARAM_MESSAGE_2_CARRY_2};
 use rayon::{prelude::*, join};
-use tfhe::integer::ciphertext::IntegerCiphertext;
-use tfhe::shortint::parameters::PARAM_MESSAGE_2_CARRY_2;
 use std::time::{Instant, Duration};
 use crate::logging;
 
 type Ciphertext = BaseRadixCiphertext<CiphertextBase<KeyswitchBootstrap>>;
 
 pub fn run(s_clear: &mut Vec<u64>, b_clear: &mut Vec<u64>, _NUM_BLOCK: usize) {
-    let (client_key, server_key) = gen_keys_radix(&PARAM_MESSAGE_2_CARRY_2, _NUM_BLOCK);
+    let (client_key, server_key) = gen_keys_radix(&PARAM_MESSAGE_2_CARRY_2, _NUM_BLOCK*2);
     let mut s = Vec::with_capacity(s_clear.len());
     let mut b = Vec::with_capacity(b_clear.len());
 
-    let mut s16 = Vec::with_capacity(s_clear.len());
-    let mut b16 = Vec::with_capacity(b_clear.len());
-
     for i in 0..s_clear.len() {
         s.push(client_key.encrypt(s_clear[i]));
-        s16.push(fromNto2Nbit(&s[i], &server_key));
     }
 
     for i in 0..s_clear.len() {
         b.push(client_key.encrypt(b_clear[i]));
-        b16.push(fromNto2Nbit(&b[i], &server_key));
     }
 
     let now = Instant::now();
     
-    volume_match(&mut s, &mut b, &mut s16, &mut b16, _NUM_BLOCK, &server_key, &client_key);
+    volume_match(&mut s, &mut b, _NUM_BLOCK, &server_key, &client_key);
 
     let elapsed: Duration = now.elapsed();
     logging::log("full_paral total", elapsed);
@@ -46,8 +39,6 @@ pub fn run(s_clear: &mut Vec<u64>, b_clear: &mut Vec<u64>, _NUM_BLOCK: usize) {
 pub fn volume_match(
     s : &mut Vec<Ciphertext>,
     b : &mut Vec<Ciphertext>,
-    s16 : &mut Vec<Ciphertext>,
-    b16 : &mut Vec<Ciphertext>,
     NUM_BLOCK: usize,
     server_key: &ServerKey,
     client_key: &RadixClientKey
@@ -56,10 +47,10 @@ pub fn volume_match(
     let mut S: Vec<Ciphertext>  = Vec::with_capacity(s.len()+1);
     let mut B: Vec<Ciphertext>  = Vec::with_capacity(b.len()+1);
     
-    for i in 0..s.len()+1 {
+    for _ in 0..s.len()+1 {
         S.push(server_key.create_trivial_zero_radix(NUM_BLOCK*2));
     }
-    for i in 0..b.len()+1 {
+    for _ in 0..b.len()+1 {
         B.push(server_key.create_trivial_zero_radix(NUM_BLOCK*2));
     }
 
@@ -67,8 +58,8 @@ pub fn volume_match(
     //let now = Instant::now();
 
     join(
-        || (for i in 0..s.len() {S[i+1] = add_assign(&mut S[i], &mut s[i], server_key);}), 
-        || (for i in 0..b.len() {B[i+1] = add_assign(&mut B[i],&mut b[i], server_key);})
+        || (for i in 0..s.len() {S[i+1] = add(&mut S[i], &mut s[i], server_key);}), 
+        || (for i in 0..b.len() {B[i+1] = add(&mut B[i],&mut b[i], server_key);})
     );
 
     //let elapsed = now.elapsed();
@@ -85,16 +76,15 @@ pub fn volume_match(
     
     // Calculate new s and b <- Parallalise this
     //let now = Instant::now();
-    
     join(
         ||{
-            *s = (s16, &mut S).into_par_iter()
-                .map(|(x, y)| min(x, y, &L, &server_key))
+            *s = (&mut s.clone(), &mut S).into_par_iter()
+                .map(|(x, y)| calc_order(x, y, &L, &server_key))
                 .collect();
         },
         ||{
-            *b = (b16, &mut B).into_par_iter()
-                .map(|(x, y)| min(x, y, &L, &server_key))
+            *b = (&mut b.clone(), &mut B).into_par_iter()
+                .map(|(x, y)| calc_order(x, y, &L, &server_key))
                 .collect();
         }
     );
@@ -103,24 +93,7 @@ pub fn volume_match(
     //logging::log("full_paral loop", elapsed);
 }
 
-fn fromNto2Nbit(x: &Ciphertext, server_key: &ServerKey) -> Ciphertext
-{
-    let NUM_BLOCK: usize = x.blocks().len();
-    let temp: Ciphertext = server_key.create_trivial_zero_radix(NUM_BLOCK);
-
-    let mut u = Vec::new();
-    u.extend_from_slice(x.blocks());
-    u.extend_from_slice(temp.blocks());
-
-    IntegerCiphertext::from_blocks(u)
-}
-
-fn from2NtoNbit(x: &mut Ciphertext) -> Ciphertext
-{
-    IntegerCiphertext::from_blocks(x.blocks()[0..x.blocks().len()/2].to_vec())
-}
-
-fn add_assign(
+fn add(
     a: &mut Ciphertext,
     b: &mut Ciphertext,
     server_key: &ServerKey
@@ -131,7 +104,7 @@ fn add_assign(
     server_key.unchecked_add(a, &b)
 }
 
-fn min(
+fn calc_order(
     x16: &mut Ciphertext,
     X: &mut Ciphertext,
     L: &Ciphertext,
@@ -144,7 +117,5 @@ fn min(
     let mut m: Ciphertext = server_key.unchecked_sub(L, &t);
     
     server_key.full_propagate_parallelized(&mut m);
-    *x16 = server_key.unchecked_min_parallelized(x16, &m);
-
-    from2NtoNbit(x16)
+    server_key.unchecked_min_parallelized(x16, &m)
 }
